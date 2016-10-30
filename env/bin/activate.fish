e082637b56802e4932f326ed34db5e6a4129357c
root <root@benatcost.student.rit.edu>
2016-10-30 08:48:45 -0400

    Setup webserver files

--- env/bin/activate.fish
+++ env/bin/activate.fish
@@ -36,7 +36,7 @@ end
 # Unset irrelevant variables.
 deactivate nondestructive
 
-set -gx VIRTUAL_ENV "/Users/spartan2194/Documents/Projects/BenAtCost.tech/env"
+set -gx VIRTUAL_ENV "/root/BenAtCost.tech/env"
 
 set -gx _OLD_VIRTUAL_PATH $PATH
 set -gx PATH "$VIRTUAL_ENV/bin" $PATH
---
+++ wsgi.py
@@ -0,0 +1,7 @@
+#from app import application
+from benatcost import app as application
+
+if __name__ == "__main__":
+    	#app.run()
+	#app.run(host='0.0.0.0')
+	application.run()
